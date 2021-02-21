7a55c145a795d285fbe2807cc23ae2fdf08f7987
valora <932942491@qq.com>
2021-02-21 15:33:22 +0800

    完善关于页面

--- app/src/main/java/com/yellowzero/listen/activity/AboutActivity.java
+++ app/src/main/java/com/yellowzero/listen/activity/AboutActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
@@ -13,15 +14,19 @@ import com.yellowzero.listen.R;
 
 public class AboutActivity extends AppCompatActivity {
 
+    private WebView webView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
         setSupportActionBar(findViewById(R.id.toolbar));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        WebView webView  = findViewById(R.id.webView);
+        webView  = findViewById(R.id.webView);
         ProgressBar pbLoad  = findViewById(R.id.pbLoad);
-        webView.getSettings().setAppCacheEnabled(false);
+        webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl("http://chenvivi.gitee.io/yellowzero/index.html");
         webView.setWebViewClient(new WebViewClient(){
             @Override
@@ -31,6 +36,19 @@ public class AboutActivity extends AppCompatActivity {
                 webView.setVisibility(View.VISIBLE);
             }
         });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView == null) {
+            super.onBackPressed();
+            return;
+        }
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 
     @Override
